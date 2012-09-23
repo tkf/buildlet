359189acfdb0c98f21dad3c66d4ba149fcc70786
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-24 00:40:31 +0200

    Quickly implement buildlet.runner.ipython

--- buildlet/runner/ipython.py
+++ buildlet/runner/ipython.py
@@ -14,3 +14,62 @@ in `IPython Documentation`_.
    http://ipython.org/ipython-doc/dev/
 
 """
+
+import itertools
+
+import networkx as nx
+from IPython import parallel
+
+
+def run(task):
+    task.pre_run()
+    try:
+        if task.is_finished():
+            task.load()
+        else:
+            run_parallel(task)
+    finally:
+        task.post_run()
+
+
+def run_parallel(task):
+    client = parallel.Client()
+    view = client.load_balanced_view()
+    (root, G, jobs) = create_tree(task)
+    results = submit_jobs(view, G, jobs)
+    view.wait(results.values())
+    return results[root].get()
+
+
+def create_tree(task):
+    G = nx.DiGraph()
+    jobs = {}
+    counter = itertools.count()
+
+    def creator(i, t):
+        jobs[i] = t
+        for p in t.get_parents():
+            k = counter()
+            G.add_node(k)
+            G.add_edge(i, k)
+            creator(k, p)
+
+    root = counter()
+    creator(counter(), task)
+    return (root, G, jobs)
+
+
+def run_task(task):
+    task.run()
+    # TBD: Should I return task object?
+    # return task
+
+
+def submit_jobs(view, G, jobs):
+    """Submit jobs via client where G describes dependencies."""
+    results = {}
+    for node in nx.topological_sort(G):
+        deps = [results[n] for n in G.predecessors(node)]
+        with view.temp_flags(after=deps):
+            results[node] = view.apply_async(run_task, jobs[node])
+    return results
