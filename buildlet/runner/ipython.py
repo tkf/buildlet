cf96a75c6882f912bb583ff488d3a8665f62b580
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-24 00:10:28 +0200

    Add buildlet/runner/ipython.py (no code)

---
+++ buildlet/runner/ipython.py
@@ -0,0 +1,16 @@
+"""
+Task runner using IPython parallel interface.
+
+See `The IPython task interface`_ and `IPython Documentation`_
+in `IPython Documentation`_.
+
+.. _The IPython task interface:
+   http://ipython.org/ipython-doc/dev/parallel/parallel_task.html
+
+.. _DAG Dependencies:
+   http://ipython.org/ipython-doc/dev/parallel/dag_dependencies.html
+
+.. _IPython Documentation:
+   http://ipython.org/ipython-doc/dev/
+
+"""
