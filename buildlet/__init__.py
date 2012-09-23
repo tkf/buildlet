17af81742f5bf6473155837c655506f4509a4273
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-23 19:28:57 +0200

    Add buildlet/__init__.py

---
+++ buildlet/__init__.py
@@ -0,0 +1,11 @@
+# [[[cog import cog; cog.outl('"""\n%s\n"""' % file('../README.rst').read())]]]
+"""
+Buildlet - build tool like functionality as a Python module
+===========================================================
+
+"""
+# [[[end]]]
+
+__author__ = "Takafumi Arakaki"
+__version__ = "0.0.1.dev0"
+__license__ = 'BSD License'
