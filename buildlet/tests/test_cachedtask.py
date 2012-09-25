6bbb6961ca66dc792b72e25a27bec32a72a5629e
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-25 23:38:54 +0200

    Assert number of load method calls in TestCachedTask

--- buildlet/tests/test_cachedtask.py
+++ buildlet/tests/test_cachedtask.py
@@ -26,6 +26,7 @@ class TestCachedTask(TestSimpleTask):
         for i in range(2):
             self.runner.run(self.task)
             self.assert_run_num(1)
+            self.assert_run_num(1 + i, 0, func='load')
             self.assert_run_num(2 + i, 1, func='pre_run')
             self.assert_run_num(2 + i, 1, func='post_success_run')
             self.assert_run_num(0, func='post_error_run')
@@ -37,6 +38,7 @@ class TestCachedTask(TestSimpleTask):
 
         self.assertRaises(AssertionError, self.assert_run_num, 1)
         self.assert_run_num(2, 1)
+        self.assert_run_num(0, 1, func='load')
         self.assert_run_num(2, 2, func='pre_run')
         self.assert_run_num(2, 2, func='post_success_run')
         self.assert_run_num(0, func='post_error_run')
@@ -49,13 +51,22 @@ class TestCachedTask(TestSimpleTask):
         self.runner.run(self.task)
 
         self.assertRaises(AssertionError, self.assert_run_num, 1)
+
+        # self.task and ptask must have same call counts
         for func in ['run', 'pre_run', 'post_success_run']:
             self.assertEqual(self.task.get_counter(func), 2)
             self.assertEqual(ptask.get_counter(func), 2)
+        self.assertEqual(self.task.get_counter('load'), 0)
+        self.assertEqual(ptask.get_counter('load'), 0)
+
+        # check other parents
         for other in self.task.get_parents()[1:]:
             self.assertEqual(other.get_counter('run'), 1)
+            self.assertEqual(other.get_counter('load'), 1)
             self.assertEqual(other.get_counter('pre_run'), 2)
             self.assertEqual(other.get_counter('post_success_run'), 2)
+
+        # finally, there should be no post_error_run call for all tasks
         self.assert_run_num(0, func='post_error_run')
 
     def test_rerun_new_instance(self):
