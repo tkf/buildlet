2b0ecd94042080f35ddc6c78d8f94a6c4a087759
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-29 13:41:20 +0200

    Extract body of Simple.run into primitive_run

--- buildlet/runner/simple.py
+++ buildlet/runner/simple.py
@@ -4,27 +4,29 @@ class SimpleRunner(object):
     Simple blocking task runner.
     """
 
-    @classmethod
-    def run(cls, task):
+    def run(self, task):
         """
         Simple blocking task runner.
 
         Run `task` and its unfinished ancestors.
 
         """
-        task.pre_run()
-        try:
-            if task.is_finished():
-                task.load()
-            else:
-                cls.run_parent(task)
-                task.run()
-            task.post_success_run()
-        except Exception as e:
-            task.post_error_run(e)
-            raise
+        primitive_run(task, self.run_parent)
 
-    @classmethod
-    def run_parent(cls, task):
+    def run_parent(self, task):
         for parent in task.get_parents():
-            cls.run(parent)
+            self.run(parent)
+
+
+def primitive_run(task, run_parent):
+    task.pre_run()
+    try:
+        if task.is_finished():
+            task.load()
+        else:
+            run_parent(task)
+            task.run()
+        task.post_success_run()
+    except Exception as e:
+        task.post_error_run(e)
+        raise
