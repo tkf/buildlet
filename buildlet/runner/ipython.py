64f106c9babb0c692f342f575b78adadd600a985
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-29 05:44:16 +0200

    Rewrite runner.ipython using class

--- buildlet/runner/ipython.py
+++ buildlet/runner/ipython.py
@@ -20,56 +20,50 @@ import itertools
 import networkx as nx
 from IPython import parallel
 
-
-def run(task):
-    task.pre_run()
-    try:
-        if task.is_finished():
-            task.load()
-        else:
-            run_parallel(task)
-    finally:
-        task.post_run()
+from .simple import SimpleRunner
 
 
-def run_parallel(task):
-    client = parallel.Client()
-    view = client.load_balanced_view()
-    (root, G, jobs) = create_tree(task)
-    results = submit_jobs(view, G, jobs)
-    view.wait(results.values())
-    return results[root].get()
+class IPythonParallelRunner(SimpleRunner):
 
+    @classmethod
+    def run_parent(cls, task):
+        client = parallel.Client()
+        view = client.load_balanced_view()
+        (root, G, jobs) = cls.create_tree(task)
+        results = cls.submit_jobs(view, G, jobs)
+        view.wait(results.values())
+        return results[root].get()
 
-def create_tree(task):
-    G = nx.DiGraph()
-    jobs = {}
-    counter = itertools.count()
+    @staticmethod
+    def create_tree(task):
+        G = nx.DiGraph()
+        jobs = {}
+        counter = itertools.count()
 
-    def creator(i, t):
-        jobs[i] = t
-        for p in t.get_parents():
-            k = counter()
-            G.add_node(k)
-            G.add_edge(i, k)
-            creator(k, p)
+        def creator(i, t):
+            jobs[i] = t
+            for p in t.get_parents():
+                k = counter()
+                G.add_node(k)
+                G.add_edge(i, k)
+                creator(k, p)
 
-    root = counter()
-    creator(counter(), task)
-    return (root, G, jobs)
+        root = counter()
+        creator(counter(), task)
+        return (root, G, jobs)
+
+    @staticmethod
+    def submit_jobs(cls, view, G, jobs):
+        """Submit jobs via client where G describes dependencies."""
+        results = {}
+        for node in nx.topological_sort(G):
+            deps = [results[n] for n in G.predecessors(node)]
+            with view.temp_flags(after=deps):
+                results[node] = view.apply_async(run_task, jobs[node])
+        return results
 
 
 def run_task(task):
-    task.run()
+    SimpleRunner.run(task)
     # TBD: Should I return task object?
     # return task
-
-
-def submit_jobs(view, G, jobs):
-    """Submit jobs via client where G describes dependencies."""
-    results = {}
-    for node in nx.topological_sort(G):
-        deps = [results[n] for n in G.predecessors(node)]
-        with view.temp_flags(after=deps):
-            results[node] = view.apply_async(run_task, jobs[node])
-    return results
