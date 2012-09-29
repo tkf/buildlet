57ec969076efb4f4d43238be39fc2676d01aa23a
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-29 23:51:42 +0200

    Run test_cachedtask* with real mock

--- buildlet/tests/test_with_real_mock.py
+++ buildlet/tests/test_with_real_mock.py
@@ -1,12 +1,23 @@
 """
-Do :class:`TestSimpleTask` with the real mock module.
+Test with the real mock module.
 """
 
 import mock
 
 # Avoid importing test case at top-level to duplicated test
 from . import test_simple
+from . import test_cachedtask
+from . import test_cachedtask_with_directory
 
 
 class TestSimpleTaskWithRealMock(test_simple.TestSimpleTask):
+    MockClass = mock.Mock
+
+
+class TestCachedTaskWithRealMock(test_cachedtask.TestCachedTask):
+    MockClass = mock.Mock
+
+
+class TestCachedTaskWithDirectoryAndWithRealMock(
+        test_cachedtask_with_directory.TestCachedTaskWithDirectory):
     MockClass = mock.Mock
