79606d48f5bfaed90b5dc766732913eff6af85cb
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-30 02:44:58 +0200

    Fix TestDumpedMockTask*.test_rerun_new_instance

--- buildlet/tests/test_dumpedmocktask.py
+++ buildlet/tests/test_dumpedmocktask.py
@@ -52,43 +52,16 @@ class TestDumpedMockTask(test_cachedtask.TestCachedTask):
     def test_rerun_new_instance(self):
         self.test_simple_run()
         self.task = self.TaskClass(**self.get_taskclass_kwds())
-
-        for func in self.TaskClass.mock_methods:
-            self.assert_run_num(0, func=func)
-
-        def getmocks(t):
-            return [t.mock] + [p.mock for p in t.get_parents()]
-
-        def setmocks(t, mocks):
-            t.mock = mocks[0]
-            for (p, m) in zip(t.get_parents(), mocks[1:]):
-                p.mock = m
-
-        newmocks = getmocks(self.task)
         self.runner.run(self.task)
-        loadedmocks = getmocks(self.task)
-        setmocks(self.task, newmocks)
 
-        for (nm, lm) in zip(newmocks[1:], loadedmocks[1:]):
-            assert nm is lm, "Mocks for the parent task should not be loaded."
-        pnum_is_zero = 0  # see TestCachedTask
+        pnum_is_zero = 0  # See TestCachedTask
 
-        # Until `run`, the numbers are the same as in TestCachedTask
-        self.assert_run_num(0, pnum_is_zero)
-        self.assert_run_num(1, pnum_is_zero, func='load')
-        self.assert_run_num(1, pnum_is_zero, func='pre_run')
-
-        # New mock is not touched at all after `run`/`load`
-        self.assert_run_num(0, pnum_is_zero, func='post_success_run')
-        self.assert_run_num(0, pnum_is_zero, func='post_error_run')
-
-        setmocks(self.task, loadedmocks)
+        # root_num is +1 than TestCachedTask to count the calls in old
+        # instance.
         self.assert_run_num(1, pnum_is_zero)
-        self.assert_run_num(0, pnum_is_zero, func='load')
-        self.assert_run_num(1, pnum_is_zero, func='pre_run')
-
-        # post_success_run mock is called in the first run and the
-        # second run.
+        self.assert_run_num(1, pnum_is_zero, func='load')
+        self.assert_run_num(2, pnum_is_zero, func='pre_run')
         self.assert_run_num(2, pnum_is_zero, func='post_success_run')
 
+        # post_error_run is never called anyway
         self.assert_run_num(0, pnum_is_zero, func='post_error_run')
