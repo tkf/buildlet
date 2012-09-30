7b26bf6544d89c67c12f036f23cc96ac7369cf77
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-30 03:22:01 +0200

    Add TestingMultiprocessingRunner

--- buildlet/tests/test_dumpedmocktask_directory_multiprocessing.py
+++ buildlet/tests/test_dumpedmocktask_directory_multiprocessing.py
@@ -3,6 +3,14 @@ from ..runner.multiprocessingpool import MultiprocessingRunner
 from . import test_dumpedmocktask_directory
 
 
+class TestingMultiprocessingRunner(MultiprocessingRunner):
+
+    def wait_tasks(self):
+        super(TestingMultiprocessingRunner, self).wait_tasks()
+        for task in self.nodetaskmap.values():
+            task.load_mock()
+
+
 class TestDumpedMockTaskDirectoryMultiprocessing(
         test_dumpedmocktask_directory.TestDumpedMockTaskDirectory):
-    RunnerClass = MultiprocessingRunner
+    RunnerClass = TestingMultiprocessingRunner
