8f4c8760dd5f6f21b1c59579332a3c81fa58ed13
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-01 01:30:15 +0200

    Document utility functions in buildlet.runner

--- buildlet/runner/__init__.py
+++ buildlet/runner/__init__.py
@@ -10,6 +10,9 @@ _namemodmap = dict(
 
 
 def getrunner(classname):
+    """
+    Get a runner class named `classname`.
+    """
     import sys
     module = 'buildlet.runner.{0}'.format(_namemodmap[classname])
     __import__(module)
@@ -17,10 +20,20 @@ def getrunner(classname):
 
 
 def listrunner():
+    """
+    Get a list of runner class names (a list of strings).
+    """
     return list(_namemodmap)
 
 
 def run(classname, task, *args, **kwds):
+    """
+    Run `task` using runner named `classname`.
+
+    Rest of the arguments are passed to the runner class.
+    Return the instance of the used runner class.
+
+    """
     runner = getrunner(classname)(*args, **kwds)
     runner.run(task)
     return runner
