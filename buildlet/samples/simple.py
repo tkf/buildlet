512f8120c764a95648f0bae1332bd7ee920c8091
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-01 23:49:37 +0200

    Rename: SimpleCachedRootTask -> SimpleCacheableRootTask

--- buildlet/samples/simple.py
+++ buildlet/samples/simple.py
@@ -9,12 +9,12 @@ class SimpleCacheableTask(BaseSimpleCacheableTask):
         print "Running {0}...".format(self)
 
 
-class SimpleCachedRootTask(SimpleCacheableTask):
+class SimpleCacheableRootTask(SimpleCacheableTask):
 
     num_parents = 3
 
     def __init__(self, **kwds):
-        super(SimpleCachedRootTask, self).__init__(**kwds)
+        super(SimpleCacheableRootTask, self).__init__(**kwds)
 
         # --clear-before-run
         for i in range(self.num_parents):
@@ -33,7 +33,7 @@ class SimpleCachedRootTask(SimpleCacheableTask):
 
 
 def run_simple_task(runner_class, **kwds):
-    task = SimpleCachedRootTask(**kwds)
+    task = SimpleCacheableRootTask(**kwds)
     print "Runner:", runner_class
     runner = run(runner_class, task)
     return (runner, task)
@@ -44,7 +44,7 @@ def main(args=None):
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('--basepath', default="tmp")
     parser.add_argument('--num-parents', type=int,
-                        default=SimpleCachedRootTask.num_parents)
+                        default=SimpleCacheableRootTask.num_parents)
     parser.add_argument('--runner-class', choices=sorted(listrunner()),
                         default='SimpleRunner')
     parser.add_argument('--clear-before-run', '-c', default=False,
